a17e340ba2f4313e5b49d1dddc3bc7ef71129a9a
joker1007 <kakyoin.hierophant@gmail.com>
2016-05-25 02:56:22 +0900

    Refactoring

--- lib/yaml_vault.rb
+++ lib/yaml_vault.rb
@@ -4,30 +4,50 @@ require 'erb'
 require 'active_support'
 
 module YamlVault
-  class << self
-    def encrypt_yaml(passphrase, sign_passphrase, yaml, keys, salt: nil, cipher: "aes-256-cbc", digest: "SHA256")
-      process_yaml(passphrase, sign_passphrase, yaml, keys, salt: salt.to_s, cipher: cipher, digest: digest) do |cryptor, data|
+  class Main
+    def initialize(yaml, keys, encryptor_name = nil, passphrase: nil, sign_passphrase: nil, salt: nil, cipher: "aes-256-cbc", digest: "SHA256")
+      @yaml = yaml
+      @keys = keys
+      @encryptor = get_encryptor(encryptor_name)
+
+      @passphrase = passphrase
+      @sign_passphrase = sign_passphrase
+      @salt = salt.to_s
+      @cipher = cipher
+      @digest = digest
+    end
+
+    def encrypt_yaml
+      process_yaml do |cryptor, data|
         do_process(cryptor, data, :encrypt)
       end
     end
 
-    def decrypt_yaml(passphrase, sign_passphrase, yaml, keys, salt: nil, cipher: "aes-256-cbc", digest: "SHA256")
-      process_yaml(passphrase, sign_passphrase, yaml, keys, salt: salt.to_s, cipher: cipher, digest: digest) do |cryptor, data|
+    def decrypt_yaml
+      process_yaml do |cryptor, data|
         do_process(cryptor, data, :decrypt)
       end
     end
 
     private
 
-    def process_yaml(passphrase, sign_passphrase, yaml, keys, salt:, cipher:, digest:)
-      cryptor = ValueCryptor.new(passphrase, sign_passphrase, salt, cipher, digest)
-      data = YAML.load(ERB.new(File.read(yaml)).result)
-      keys.each do |key|
+    def get_encryptor(name)
+      if name == "simple"
+        ValueCryptor::Simple
+      else
+        ValueCryptor::Simple
+      end
+    end
+
+    def process_yaml
+      value_cryptor = @encryptor.new(@passphrase, @sign_passphrase, @salt, @cipher, @digest)
+      data = YAML.load(ERB.new(File.read(@yaml)).result)
+      @keys.each do |key|
         target = key.inject(data) do |t, part|
           t[part]
         end
 
-        vault_data = yield cryptor, target
+        vault_data = yield value_cryptor, target
 
         target_parent = key[0..-2].inject(data) do |t, part|
           t[part]
@@ -59,28 +79,30 @@ module YamlVault
         cryptor.send(method, data)
       end
     end
-  end
 
-  class ValueCryptor
-    def initialize(passphrase, sign_passphrase, salt, cipher, digest, key_size = 64)
-      key = ActiveSupport::KeyGenerator.new(passphrase).generate_key(salt, key_size)
-      signature_key = ActiveSupport::KeyGenerator.new(sign_passphrase).generate_key(salt, key_size) if sign_passphrase
+    module ValueCryptor
+      class Simple
+        def initialize(passphrase, sign_passphrase, salt, cipher, digest, key_size = 64)
+          key = ActiveSupport::KeyGenerator.new(passphrase).generate_key(salt, key_size)
+          signature_key = ActiveSupport::KeyGenerator.new(sign_passphrase).generate_key(salt, key_size) if sign_passphrase
 
-      if signature_key
-        @cryptor = ActiveSupport::MessageEncryptor.new(key, signature_key, cipher: cipher, digest: digest)
-      else
-        @cryptor = ActiveSupport::MessageEncryptor.new(key, cipher: cipher, digest: digest)
+          if signature_key
+            @cryptor = ActiveSupport::MessageEncryptor.new(key, signature_key, cipher: cipher, digest: digest)
+          else
+            @cryptor = ActiveSupport::MessageEncryptor.new(key, cipher: cipher, digest: digest)
+          end
+        end
+
+        def encrypt(value)
+          @cryptor.encrypt_and_sign(value)
+        end
+
+        def decrypt(value)
+          @cryptor.decrypt_and_verify(value)
+        end
       end
     end
 
-    def encrypt(value)
-      @cryptor.encrypt_and_sign(value)
-    end
-
-    def decrypt(value)
-      @cryptor.decrypt_and_verify(value)
-    end
+    private_constant :ValueCryptor
   end
-
-  private_constant :ValueCryptor
 end
--- spec/yaml_vault_spec.rb
+++ spec/yaml_vault_spec.rb
@@ -4,7 +4,7 @@ describe YamlVault do
   describe ".encrypt_yaml" do
     context "use sign_passphrase" do
       it 'generate encrypt yaml' do
-        encrypted = YAML.load(YamlVault.encrypt_yaml("testpassphrase", "signpassphrase", File.expand_path("../sample.yml", __FILE__), [["vault"]]))
+        encrypted = YAML.load(YamlVault::Main.new(File.expand_path("../sample.yml", __FILE__), [["vault"]], passphrase: "testpassphrase", sign_passphrase: "signpassphrase").encrypt_yaml)
         aggregate_failures do
           expect(encrypted["vault"]["secret_data"]).not_to eq "hogehoge"
           expect(encrypted["vault"]["secrets"][0]).not_to eq 1
@@ -19,7 +19,7 @@ describe YamlVault do
 
     context "use no sign_passphrase" do
       it 'generate encrypt yaml' do
-        encrypted = YAML.load(YamlVault.encrypt_yaml("testpassphrase", nil, File.expand_path("../sample.yml", __FILE__), [["vault"]]))
+        encrypted = YAML.load(YamlVault::Main.new(File.expand_path("../sample.yml", __FILE__), [["vault"]], passphrase: "testpassphrase").encrypt_yaml)
         aggregate_failures do
           expect(encrypted["vault"]["secret_data"]).not_to eq "hogehoge"
           expect(encrypted["vault"]["secrets"][0]).not_to eq 1
@@ -35,7 +35,7 @@ describe YamlVault do
 
   describe ".decrypt_yaml" do
     it 'generate decrypt yaml' do
-      decrypted = YAML.load(YamlVault.decrypt_yaml("testpassphrase", "signpassphrase", File.expand_path("../encrypted_sample.yml", __FILE__), [["vault"]]))
+      decrypted = YAML.load(YamlVault::Main.new(File.expand_path("../encrypted_sample.yml", __FILE__), [["vault"]], passphrase: "testpassphrase", sign_passphrase: "signpassphrase").decrypt_yaml)
       aggregate_failures do
         expect(decrypted["vault"]["secret_data"]).to eq "hogehoge"
         expect(decrypted["vault"]["secrets"][0]).to eq 1
@@ -50,7 +50,7 @@ describe YamlVault do
     context "different salt" do
       it do
         expect {
-          YamlVault.decrypt_yaml("testpassphrase", "signpassphrase", File.expand_path("../encrypted_sample.yml", __FILE__), [["vault"]], salt: "dummy")
+          YamlVault::Main.new(File.expand_path("../encrypted_sample.yml", __FILE__), [["vault"]], passphrase: "testpassphrase", sign_passphrase: "signpassphrase", salt: "dummy").decrypt_yaml
         }.to raise_error(ActiveSupport::MessageVerifier::InvalidSignature)
       end
     end
@@ -58,7 +58,7 @@ describe YamlVault do
     context "different sign_passphrase" do
       it do
         expect {
-          YamlVault.decrypt_yaml("testpassphrase", "invalidsignpassphrase", File.expand_path("../encrypted_sample.yml", __FILE__), [["vault"]], salt: "dummy")
+          YamlVault::Main.new(File.expand_path("../encrypted_sample.yml", __FILE__), [["vault"]], passphrase: "testpassphrase", sign_passphrase: "invalidsignpassphrase", salt: "dummy").decrypt_yaml
         }.to raise_error(ActiveSupport::MessageVerifier::InvalidSignature)
       end
     end
@@ -66,7 +66,7 @@ describe YamlVault do
     context "different passphrase" do
       it do
         expect {
-          YamlVault.decrypt_yaml("invalidpassphrase", "signpassphrase", File.expand_path("../encrypted_sample.yml", __FILE__), [["vault"]], salt: "dummy")
+          YamlVault::Main.new(File.expand_path("../encrypted_sample.yml", __FILE__), [["vault"]], passphrase: "invalidpassphrase", sign_passphrase: "signpassphrase", salt: "dummy").decrypt_yaml
         }.to raise_error(ActiveSupport::MessageVerifier::InvalidSignature)
       end
     end
