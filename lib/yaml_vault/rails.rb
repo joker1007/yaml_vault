4a4b31f7df83cd7fdd2dc98972496c961e18ef18
abicky <takeshi.arabiki@gmail.com>
2017-12-15 15:44:19 +0900

    Support Rails 5.1
    In Rails 5.1, nested keys are also symbolized
    cf. https://github.com/rails/rails/pull/26929

--- lib/yaml_vault/rails.rb
+++ lib/yaml_vault/rails.rb
@@ -9,7 +9,15 @@ module YamlVault
           if File.exist?(yaml)
             all_secrets = YamlVault::Main.from_content(IO.read(yaml), keys, cryptor_name, **options).decrypt_hash
             env_secrets = all_secrets[::Rails.env]
-            secrets.merge!(env_secrets.symbolize_keys) if env_secrets
+            if env_secrets
+              if Gem::Version.new(::Rails::VERSION::STRING) >= Gem::Version.new("5.1")
+                # In Rails 5.1, nested keys are also symbolized
+                # cf. https://github.com/rails/rails/pull/26929
+                secrets.merge!(env_secrets.deep_symbolize_keys)
+              else
+                secrets.merge!(env_secrets.symbolize_keys)
+              end
+            end
           end
 
           # Fallback to config.secret_key_base if secrets.secret_key_base isn't set
